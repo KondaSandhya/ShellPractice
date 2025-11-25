036a4fd22a0fc6a0667feccfd12a21158ae47539
Sandhya <sandhyareddy5e4@gmail.com>
2025-11-25 11:49:35 +0530

    datatypes

--- 5-variables.sh
+++ 5-variables.sh
@@ -3,5 +3,7 @@
 echo "Enter your username:"
 read username
 echo "enter your password:"
+#Here -s flag reads the password silently means it wont display anything while typing pwd
 read -s password
+#Its not best practice to display the password on the screen
 echo "password for user $username is set as : $password"
---
+++ 6-dataTypes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+timestamp=$(date)
+echo "Todays date is: $timestamp"
