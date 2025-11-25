157c1976b28fbf6aa8b65f7db7fd2273ede47d38
Sandhya <sandhyareddy5e4@gmail.com>
2025-11-25 11:33:49 +0530

    simple Conversation

---
+++ 2.conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, how are you?"
+echo "Suresh: Hi Ramesh, I'm good! How about you?"
+echo "Ramesh: I'm doing well, thanks for asking."
+echo "Suresh: Have you completed the project we were working on?"
+echo "Ramesh: Yes, I finished it yesterday. I'll send you the details."
