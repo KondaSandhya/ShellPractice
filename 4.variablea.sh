6754b2c85c157777cbd24b27f009ef11b7f39da1
Sandhya <sandhyareddy5e4@gmail.com>
2025-11-25 11:40:42 +0530

    variables

---
+++ 4.variablea.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2, how are you?"
+echo "$Person2: Hi $Person1, I'm good! How about you?"
+echo "$Person1: I'm doing well, thanks for asking."
+echo "$Person2: Have you completed the project we were working on?"
+echo "$Person1: Yes, I finished it yesterday. I'll send you the details."
