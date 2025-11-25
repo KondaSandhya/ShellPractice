3e919d735531ce3f3f71acd2da6b79bc823cae10
Sandhya <sandhyareddy5e4@gmail.com>
2025-11-25 12:07:54 +0530

    Arrays

---
+++ 7-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+movies=("Inception" "The Matrix" "Interstellar" "The Dark Knight" "Pulp Fiction")
+echo "First movie: ${movies[0]}"
+echo "second movie: ${movies[1]}"
+echo "All Movies: ${movies[@]}"
