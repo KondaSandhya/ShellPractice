63f61eab27fa2f4ae9daa1abaf20c78e529df2e5
Sandhya <sandhyareddy5e4@gmail.com>
2025-11-25 11:59:40 +0530

    datatypes

--- 6-dataTypes.sh
+++ 6-dataTypes.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#sample addition we are trying
+num1=$1
+num2=$2
+
+sum=$($num1+$num2)
+echo "The sum of $num1 and $num2 is: $sum"
+
+#printing the date
 timestamp=$(date)
 echo "Todays date is: $timestamp"
