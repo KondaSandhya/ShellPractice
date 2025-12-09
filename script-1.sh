ae485553c6fb516b60bab4611d4c4ce5db79cb02
Sandhya <sandhyareddy5e4@gmail.com>
2025-12-09 11:01:02 +0530

    calling script inside script

--- script-1.sh
+++ script-1.sh
@@ -5,6 +5,6 @@ echo "Hello from: $0"
 
 echo "PID of the script is: $$"
 
-#./script-2.sh
+#./script-2.sh -- if we use this script 2 doeant have access to the variables from script1 and pid will be different
 
-source ./script-2.sh
+source ./script-2.sh # -- this will give access to the variables and pid will be same as script1
