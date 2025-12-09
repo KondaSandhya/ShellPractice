4d9212373782133c72977bf5547914becb941e2f
Sandhya <sandhyareddy5e4@gmail.com>
2025-12-09 10:34:52 +0530

    deleting old log files

--- 18-delete-old-log-files.sh
+++ 18-delete-old-log-files.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 while IFS= read -r line; do
     echo "Deleting the file: $line"
-done <<<find . -name "*.log" -type f -mtime +9 
+done <<< find . -name "*.log" -type f -mtime +9 
