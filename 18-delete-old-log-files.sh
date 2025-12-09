11c52d254afffa691007d82da4ce123ef124c27f
Sandhya <sandhyareddy5e4@gmail.com>
2025-12-09 10:41:28 +0530

    deleting old log files

--- 18-delete-old-log-files.sh
+++ 18-delete-old-log-files.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+# we can also use IFS= to avoid word splitting isses
+# find . -name "*.log" -type f -mtine +7 -exec rm -f {} \;
+# find . -name "*.log" -type f -mtime +9 -print -delete or just -delete
+
 while read -r line; do
     echo "Deleting the file: $line"
     rm -rf "$line"
